ce5d9a70d6ddf8616cb198441e86a49b0937f8d6
Katy Long <katy.long@wearecodenation.com>
2024-09-05 13:19:08 +0100

    added requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from website import create_app
 #imports the whole website folder, and the create_app() from __init__.py
 
+app = create_app() # the returned object from create_app() with all the config in
+
 if __name__ == "__main__":
-    app = create_app() # the returned object from create_app() with all the config in
     app.run(debug=True)
